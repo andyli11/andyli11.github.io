0f8a5268ff6eaa60cf334bb5203b8f140f4a8e22
Andy <jh24li@uwaterloo.ca>
2021-05-03 23:10:57 -0400

    finish projects section

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,3 @@ toggleMenu.addEventListener('click', ()=>{
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
-
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
-
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
